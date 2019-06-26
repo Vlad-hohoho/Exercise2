405414b0ea6d35462adad82fe346925c0d94615c
Vlad-hohoho <50379293+Vlad-hohoho@users.noreply.github.com>
2019-06-26 20:02:53 +0300

    Create #1.js

---
+++ #1.js
@@ -0,0 +1,32 @@
+<scripte>
+  var v, d, box[100], co =1, j, i, m, q, z, bb, ff;
+  document.print(" âåä³òü ÷èñëî \n");
+  d = promt(,0);
+  v=d;
+  for (i = 0; i < co;)
+  {
+    if (v > 10){
+        j++;
+        q=v/10; 
+        box[j]=v-q*10;
+        v=v/10;
+    } else { 
+        j++;
+        box[j]=v;
+        i++;
+    }
+  }
+ z=box[j];
+  for ( int i = 1; i <= j-1; i++){
+for(ff=1; ff<i; ff++){
+bb*=10;
+      z = z + box[j-i] * bb;
+          }
+                if (i=j){
+          if(z!=d){
+              alert("false");
+          } else { 
+              atert('true');
+          }
+      }
+</scripte>
